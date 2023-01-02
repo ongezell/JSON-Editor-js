5322e020981ae4825f4be768a7b1eb616e0b0fad
Ongezell <65968666+ongezell@users.noreply.github.com>
2023-01-02 12:54:26 -0300

    Create JSON_editor.js

---
+++ JSON_editor.js
@@ -0,0 +1,29 @@
+      $(document).ready(function() {
+        $('#process-json').click(function() {
+            var data = $('#json-input').val();
+          data = JSON.parse(data);
+
+var options = "";
+for (var i = 0; i < Object.keys(data).length; i++) {
+  options += (i + 1) + ": " + Object.keys(data)[i] + "\n";
+}
+
+var selectedArray = prompt("Select an array:\n" + options);
+
+
+          selectedArray = parseInt(selectedArray) - 1;
+          selectedArray = Object.keys(data)[selectedArray];
+          var exampleValue = data[selectedArray][0];
+          var keys = Object.keys(exampleValue);
+          console.log(keys);
+          var newValue = {};
+          for (var i = 0; i < keys.length; i++) {
+            var key = keys[i];
+            var value = prompt("Enter the value for " + key + ":");
+            newValue[key] = value;
+          }
+
+          data[selectedArray].push(newValue)
+          $('#json-input').val(JSON.stringify(data, null, 2));
+        });
+      });
